c7a476b98718d09cebf366ea1306093049e5c99e
Kensuke Nagae <kyanny@gmail.com>
2014-04-12 00:18:11 +0900

    Complete 1st tutorial

--- HelloWorld/HelloWorldViewController.h
+++ HelloWorld/HelloWorldViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface HelloWorldViewController : UIViewController
-
+@interface HelloWorldViewController : UIViewController <UITextFieldDelegate>
+- (IBAction)changeGreeting:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (copy, nonatomic) NSString *userName;
 @end
--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -13,6 +13,7 @@
 @end
 
 @implementation HelloWorldViewController
+@synthesize userName = _userName;
 
 - (void)viewDidLoad
 {
@@ -26,4 +27,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.label.text = greeting;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.textField) {
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
 @end
